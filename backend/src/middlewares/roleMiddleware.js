6dfe55e30b4e17f23e720e3e17a85c689a99f4e9
Minh Tân <165477096+TanMinh33129@users.noreply.github.com>
2026-04-13 09:25:30 +0700

    Delete backend directory

--- backend/src/middlewares/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-exports.adminOnly = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({
-      success: false,
-      message: 'Bạn không có quyền truy cập chức năng này'
-    });
-  }
-  next();
-};
